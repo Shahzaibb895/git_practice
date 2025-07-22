6a9b6d6961f7538edd806bdc98c73c9a61d4c16d
[Shahzaib Haider] <sh3440844@gmail.com>
2025-07-22 16:26:29 +0500

    modifying app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-def add(a,b):
-    return a + b
-=======
 def add(a,b,c):
     return a + b + c
-    
->>>>>>> 2e94d92b76fa0cde05164ad3c8b93ecad46a21b2
+
 def subt(a,b,c):
     return a - b - c
 
