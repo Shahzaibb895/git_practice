2325b3130def48481a4128f9f617bd2cda0d2b9b
Shahzaibb895 <sh3440844@gmail.com>
2025-07-22 15:56:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 <<<<<<< HEAD
-def add(a,b):
-    return a + b
+def add(a,b,c):
+    return a + b + c
+    
 def subt(a,b,c):
     return a - b - c
-=======
-def subt(a,b):
-    return a - b
->>>>>>> 50005be3a743f0fa118701bad559c532f79616c3
 
 def mult(a,b):
     return a * b
