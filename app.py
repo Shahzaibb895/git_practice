47379025537d1d2ae23a42f72aca92ec1e606853
[Shahzaib Haider] <sh3440844@gmail.com>
2025-07-23 21:03:41 +0500

    updating app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def add(a,b,c):
-    return a+b+c
+def add(a,b):
+    return a+b
 
-def subtract(a,b,c):
-    return a-b-c
+def subtract(a,b):
+    return a-b-
 
 def multi(a,b,c):
     return a*b*c
